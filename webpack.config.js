b34acae50e3bc00cd6412379a3c692d465819a04
Sky051 <126015395+Sky051@users.noreply.github.com>
2025-05-11 11:43:56 +0530

    Add files via upload

---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+const path = require('path');
+const webpack = require('webpack');
+const dotenv = require('dotenv');
+const env = dotenv.config().parsed || {};
+
+const envKeys = Object.keys(env).reduce((prev, next) => {
+  prev[`process.env.${next}`] = JSON.stringify(env[next]);
+  return prev;
+}, {});
+
+module.exports = {
+  entry: './src/index.jsx',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  devServer: {
+    static: './dist',
+    hot: true,
+    open: true,
+    port: 3001,
+  },
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          },
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader', 'postcss-loader'],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  plugins: [
+    new webpack.DefinePlugin(envKeys)
+  ],
+  devtool: 'inline-source-map',
+};
